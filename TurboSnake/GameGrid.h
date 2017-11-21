d9ae10415e23bb5e4dacb2ea23a4da957d5bf329
Marc Rousavy <marcrousavy@hotmail.com>
2017-11-21 15:14:57 +0100

    Implement width and height getting in game grid

--- TurboSnake/GameGrid.h
+++ TurboSnake/GameGrid.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <array>
+#include "Structures.h"
 
 /// \brief A single game field in the Snake game grid
 struct field
@@ -7,7 +8,7 @@ struct field
 	/// \brief A boolean value indicating whether this field should be painted
 	bool filled = false;
 	/// \brief The absolute position in pixels of this field
-	point abs_pos = {0,0};
+	point abs_pos = { 0,0 };
 };
 
 
@@ -18,8 +19,14 @@ class row
 private:
 	std::array<field, Width> fields_;
 public:
+	/// \brief Return the width of this row
+	static constexpr size_t width()
+	{
+		return Width;
+	}
+
 	/// \brief Get the nth field from this row
-	field operator[](const int index)
+	field& operator[](const int index)
 	{
 		return fields_[index];
 	}
@@ -32,8 +39,19 @@ class game_grid
 private:
 	std::array<row<Width>, Height> rows_;
 public:
+	/// \brief Return the width of this grid
+	static constexpr size_t width()
+	{
+		return Width;
+	}
+	/// \brief Return the height of this grid
+	static constexpr size_t height()
+	{
+		return Height;
+	}
+
 	/// \brief Get the nth row from the game grid
-	row<Width> operator[](const int index)
+	row<Width>& operator[](const int index)
 	{
 		return rows_[index];
 	}
